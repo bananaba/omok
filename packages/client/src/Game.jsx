39194ed079e6ab4623adf46b945b40fc086cf636
bananaba <200tiger1@gmail.com>
2024-03-21 10:49:29 +0900

    ✨ Omok table v0.1
    - 기본적인 오목 테이블을 만들었다

---
+++ packages/client/src/Game.jsx
@@ -0,0 +1,48 @@
+import styled from "@emotion/styled";
+import OmokTile from "./OmokTile";
+import { useState, useEffect } from "react";
+
+export default function Game() {
+  const [table, setTable] = useState(new Array(18 * 18));
+  const [turn, setTurn] = useState(false);
+  const tiles = new Array(18);
+
+  const setStone = (x, y) => {
+    return () => {
+      const copy = [...table];
+      copy[y * 18 + x] = turn ? "white" : "black";
+      setTable(copy);
+      setTurn(!turn);
+    };
+  };
+
+  for (let y = 0; y < 18; y++) {
+    const tile = [];
+    for (let x = 0; x < 18; x++)
+      tile.push(
+        <OmokTile
+          key={"tile-" + x + "-" + y}
+          pos={[x, y]}
+          stone={table[y * 18 + x]}
+          setStone={setStone(x, y)}
+        />
+      );
+    tiles[y] = <OmokTable key={"table-" + y}>{tile}</OmokTable>;
+  }
+
+  return <GameTable>{tiles}</GameTable>;
+}
+
+const GameTable = styled.div`
+  position: relative;
+  top: 32px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+const OmokTable = styled.div`
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
+`;
---
+++ packages/client/src/OmokTile.jsx
@@ -0,0 +1,114 @@
+import styled from "@emotion/styled";
+
+export default function OmokTile({ pos, stone, setStone }) {
+  const getAxis = (pos) => {
+    let horizontalClass = "horizontal";
+    let verticalClass = "vertical";
+    const axis = [];
+
+    if (pos[0] === 0) horizontalClass += " first";
+    else if (pos[0] === 17) horizontalClass += " last";
+    if (pos[1] === 0) verticalClass += " first";
+    else if (pos[1] === 17) verticalClass += " last";
+
+    axis.push(<Axis className={horizontalClass} />);
+    axis.push(<Axis className={verticalClass} />);
+
+    return axis;
+  };
+
+  const getMark = (pos) => {
+    if ((pos[0] + 4) % 6 !== 0) return null;
+    if ((pos[1] + 4) % 6 !== 0) return null;
+    return <Mark />;
+  };
+
+  const getStone = (stone) => {
+    if (!stone) return null;
+    else if (stone === "black") return <Stone className="black" />;
+    else if (stone === "white") return <Stone className="white" />;
+    else return null;
+  };
+
+  return (
+    <Tile onClick={setStone}>
+      {getAxis(pos)}
+      {getMark(pos)}
+      {getStone(stone)}
+    </Tile>
+  );
+}
+
+const Tile = styled.div`
+  position: relative;
+  width: 48px;
+  height: 48px;
+  border: none;
+  background: none;
+  overflow: hidden;
+  cursor: pointer;
+  flex-shrink: 0;
+`;
+
+const Stone = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  width: 90%;
+  height: 90%;
+  transform: translate(-50%, -50%);
+  border-radius: 50%;
+  border: solid 1px black;
+  z-index: 10;
+
+  &.black {
+    background-color: black;
+  }
+  &.white {
+    background-color: white;
+  }
+`;
+
+const Axis = styled.div`
+  position: absolute;
+  border: none;
+  background-color: black;
+
+  &.horizontal {
+    top: 50%;
+    width: 100%;
+    height: 2px;
+    transform: translate(0, -50%);
+
+    &.first {
+      left: 50%;
+    }
+    &.last {
+      left: -50%;
+    }
+  }
+  &.vertical {
+    left: 50%;
+    width: 2px;
+    height: 100%;
+    transform: translate(-50%, 0);
+
+    &.first {
+      top: 50%;
+    }
+    &.last {
+      top: -50%;
+    }
+  }
+`;
+
+const Mark = styled.div`
+  width: 15%;
+  height: 15%;
+  background-color: black;
+  border: none;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+`;
